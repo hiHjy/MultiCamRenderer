0810d705eca6562724b3494c8f24f0058657f4f1
hiHjy <huangjiyaun123@gmail.com>
2026-08-22 15:27:18 +0800

    准备程序运行上下文单例类

---
+++ include/AppRuntime.hpp
@@ -0,0 +1,15 @@
+#include "CamManager.hpp"
+class AppRuntime {
+public:
+    static AppRuntime& getInstance() {
+        static AppRuntime instance;  // 魔法在这里！
+        return instance;
+    }
+
+private:
+    AppRuntime() = default;          // 构造函数私有，禁止外部构造
+    ~AppRuntime() = default;
+    AppRuntime(const AppRuntime&) = delete;            // 禁止拷贝
+    AppRuntime& operator=(const AppRuntime&) = delete; // 禁止赋值
+	
+};
